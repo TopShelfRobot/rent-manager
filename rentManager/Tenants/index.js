4acf9f03aac42cdb7596de12c417e094c525d25b
Aaron Johnson <aarontropy@gmail.com>
2017-12-21 12:44:42 -0500

    add Tenant to API

---
+++ rentManager/Tenants/index.js
@@ -0,0 +1,24 @@
+const Query = require('./Query');
+
+const Tenants = {
+  find() {
+    const query = Query({
+      base: this.base, 
+      url: this.basePath,
+    })
+
+    Object.assign(query, { });
+
+    return query;
+  }
+}
+
+
+module.exports = base => {
+  const tenants = Object.create(Tenants);
+
+  tenants.base = base;
+  tenants.basePath = '/Tenants';
+
+  return tenants;
+}
