34ea712bc4bfb180a9f29e837529b9cb58e6d1f3
Aaron Johnson <aarontropy@gmail.com>
2017-12-31 01:45:47 -0500

    add normal query object to Properties

--- rentManager/Properties.js
+++ rentManager/Properties.js
@@ -1,10 +1,19 @@
-const utils = require('./utils')
+const Query = require('./Query');
+const utils = require('./utils');
 
 const makeFilter = (field, op, value) => {
   return [field, op, value].join(',')
 }
 
 const Properties = {
+  get() {
+    return Query({
+      base: this.base, 
+      url: this.basePath,
+    })
+  },
+  find() { return this.get(); },
+  
   async findByShortName(shortName) {
     const url = this.basePath + "?filters=" + makeFilter('ShortName', 'eq', shortName)
     return this.base.get(url).then(utils.takeFirst)
