994885a1e3a2275852a7bb01d1edc3d755a5b563
Aaron Johnson <aarontropy@gmail.com>
2018-01-01 20:25:54 -0500

    add post to Units

--- rentManager/Units.js
+++ rentManager/Units.js
@@ -16,23 +16,32 @@ const Units = {
       base: this.base, 
       url: this.basePath,
     })
-
+    
     Object.assign(query, {
       filterAmenity(amenity) {
         const sub = this._makeFilter('Text', 'eq', amenity);
         this.filter(`Amenities(${sub}).Selected`, 'eq', 'true');
-
+        
         return this;
       },
       filterUdf(udfName, op, udfValue) {
         const sub = this._makeFilter('Name', 'eq', udfName);
         this.filter(`UserDefinedValues(${sub}).Value`, op, udfValue);
-
+        
         return this;
       }
     });
-
+    
     return query;
+  },
+  
+  post() {
+    const query = Query({
+      method: 'post',
+      base: this.base, 
+      url: this.basePath,
+    })
+    
   },
 
   /**
